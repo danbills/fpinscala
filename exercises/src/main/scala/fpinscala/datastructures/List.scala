885ad524e90898656f2a5edf58015365ef09635d
Brian Zeligson <bzeligson@localytics.com>
2014-08-28 00:38:58 -0400

    chapter 3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -40,26 +40,148 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
   
-  def sum2(ns: List[Int]) = 
+  def sum2(ns: List[Int]) =
     foldRight(ns, 0)((x,y) => x + y)
   
   def product2(ns: List[Double]) = 
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => t
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(oh, t) => Cons(h, t)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0) l
+    else drop(List.tail(l), n-1)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) =>
+      if (!f(h)) l
+      else dropWhile(t, f)
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = {
+    def go(acc: List[A], itr: List[A]): List[A] = itr match {
+      case Nil => Nil
+      case Cons(h, t) => t match {
+        case Nil => acc
+        case Cons(h1, t1) => t1 match {
+          case Nil => Cons(h, acc)
+          case _ => go(Cons(h, acc), t)
+        }
+      }
+    }
+    go(List(), l)
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((i, s) => s + 1)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) =>
+      foldLeft(t, f(z, h))(f)
+  }
+
+  def sumLeft(l: List[Int]) = l match {
+    case Nil => 0
+    case _ => foldLeft(l, 0)(_ + _)
+  }
+
+  def productLeft(l: List[Double]) = l match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case _ => foldLeft(l, 1.0)(_ * _)
+  }
+
+  def sizeLeft[A](l: List[A]) = l match {
+    case Nil => 0
+    case _ => foldLeft(l, 0)((s, i) => s + 1)
+  }
+
+  def reverse[A](l: List[A]) = l match {
+    case Nil => Nil
+    case _ => foldLeft(l, List[A]())((acc, a) => Cons(a, acc))
+  }
+
+  def foldAppend[A](l1: List[A], l2: List[A]): List[A] = foldRightLeft(l1, l2)((acc, i) => Cons(i, acc))
+
+  def flatten[A](l: List[List[A]]): List[A] = foldRightLeft(l, List[A]())((acc, i) => foldAppend(i, acc))
+
+  def foldLeftRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = foldRight(reverse(l), z)(f)
+
+  def foldRightLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = foldLeft(reverse(l), z)(f)
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = foldRightLeft(l, List[B]())((acc, i) => Cons(f(i), acc))
+
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = foldLeft(l, List[B]())((acc, i) => foldAppend(acc, f(i)))
+
+  def mapAddOne(l: List[Int]): List[Int] = foldRightLeft(l, List[Int]())((acc, i) => Cons((i+1), acc))
+
+  def mapDoubleToString(l: List[Double]): List[String] = foldRightLeft(l, List[String]())((acc, i)  => Cons(i.toString, acc))
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    foldRightLeft(l, List[A]())((acc, i) => if (f(i)) Cons(i, acc) else acc)
+  }
+
+  def flatMapFilter[A](l: List[A])(f: A => Boolean): List[A] = {
+    flatMap(l)((i) => if (f(i)) List(i) else Nil)
+  }
+
+  def zipWith[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = {
+    @annotation.tailrec
+    def go(il1: List[A], il2: List[A], acc: List[A]): List[A] = il1 match {
+      case Nil => acc
+      case Cons(h, t) => il2 match {
+        case Nil => flatten(List(acc, il2))
+        case Cons(h1, t1) => go(t, t1, flatten(List(acc, List(f(h, h1)))))
+      }
+    }
+
+    go(l1, l2, List[A]())
+  }
+
+  def zipSum(l1: List[Int], l2: List[Int]): List[Int] = {
+    @annotation.tailrec
+    def go(il1: List[Int], il2: List[Int], acc: List[Int]): List[Int] = il1 match {
+      case Nil => acc
+      case Cons(h, t) => il2 match {
+        case Nil => flatten(List(acc, il2))
+        case Cons(h1, t1) => go(t, t1, flatten(List(acc, List((h + h1)))))
+      }
+    }
+
+    go(l1, l2, List[Int]())
+  }
+
+  def hasSubSequence[A](l: List[A], s: List[A]): Boolean = {
+    @annotation.tailrec
+    def go(l1: List[A], s1: List[A]): Boolean = s1 match {
+      case Nil => true
+      case Cons(sh, st) => l1 match {
+        case Nil => false
+        case Cons(lh, lt) => {
+          if (sh == lh) go(lt, st)
+          else go(lt, s)
+
+        }
+      }
+    }
+
+    go(l, s)
+  }
 }
+
